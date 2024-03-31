f9d34041acf949764ba8758471a619c9ff6a6b78
Samuel Sant Anna <samuelsantaanna@gmail.com>
2024-03-31 11:14:56 -0300

    program ready

--- src/HtmlAnalyzer.java
+++ src/HtmlAnalyzer.java
@@ -3,33 +3,33 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HtmlAnalyzer {
-    String tagRegex = "<\\/?(\\w+)>";
 
     public static void main(String[] args) throws Exception {
         String initialURL = args[0];
-        HtmlAnalyzer htmlAnalyzer = new HtmlAnalyzer();
-        String htmlText = htmlAnalyzer.getHtml(initialURL);
-        String targetText = htmlAnalyzer.findNestedText(htmlText);
+        String htmlText = HtmlAnalyzer.getHtml(initialURL);
+        String targetText = HtmlAnalyzer.findNestedText(htmlText);
         System.out.println(targetText);
     }
 
-    public String getHtml(String urlArg) throws Exception {
+    public static String getHtml(String urlArg) {
         String htmlContent = ""; // String para receber o texto html deste método
         try {
             URL url = new URL(urlArg);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection(); // abre uma conexão com a url (line 21)
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             InputStream is = conn.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             StringBuilder htmlContentBuilder = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
-                htmlContentBuilder.append(line.trim());
+                htmlContentBuilder.append(line);
+                htmlContentBuilder.append("\n");
             }
             htmlContent = htmlContentBuilder.toString();
             reader.close();
@@ -41,41 +41,40 @@ public class HtmlAnalyzer {
         return htmlContent;
     }
 
-    public String findNestedText(String htmlContent) throws Exception {
+    public static String findNestedText(String htmlContent) {
         String targetText = "";
         Stack<String> tagsStack = new Stack<>();
-        int depthCounter = 0;
         int deepestTag = 0;
-        Pattern tagPattern = Pattern.compile(tagRegex);
-        Matcher findOut = tagPattern.matcher(htmlContent);
-        int beginIndex = 0; // seta o index onde começa o texto mais profundo (line 69 (nice!))
-        int endIndex = 0; // seta o index onde termina o texto mais profundo (line 57)
-        while (findOut.find()) {
-            String tag = findOut.group();
-            String tagClear = findOut.group(1);
-            if (tag.startsWith("</")) {
-                if(!tagClear.equals(tagsStack.peek())){
-                    return "malformed HTML";
-                } 
+        Pattern tagPattern = Pattern.compile("<\\/?(\\w+)>");
+        Scanner scanner = new Scanner(htmlContent);
+        try {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
 
-                if (deepestTag == depthCounter) {
-                    endIndex = findOut.start();
-                    targetText = htmlContent.substring(beginIndex, endIndex);
+                Matcher tagMatched = tagPattern.matcher(line);
+                if (tagMatched.find()) {
+                    String tagName = tagMatched.group(1);
+                    if (line.startsWith("</")) {
+                        String lastTagInStack = tagsStack.pop();
+                        if (!lastTagInStack.equals(tagName)) {
+                            throw new Error("malformed HTML");
+                        }
+                    } else if (line.startsWith("<")) {
+                        tagsStack.push(tagName);
+                    }
+                } else {
+                    if (tagsStack.size() > deepestTag) {
+                        targetText = line;
+                        deepestTag = tagsStack.size();
+                    }
                 }
-                depthCounter--;
-                tagsStack.pop();
-            } else {
-                depthCounter++;
-                tagsStack.push(tagClear.trim());
-                if (depthCounter > deepestTag) {
-                    deepestTag = depthCounter;
-                    beginIndex = findOut.end();
-                }
-
             }
-
+        } catch (Error e) {
+            targetText = "";
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
         }
-
         return targetText;
     }
 
